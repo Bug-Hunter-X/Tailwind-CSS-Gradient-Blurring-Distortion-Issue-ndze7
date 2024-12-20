f619370b04bbc0e0e4ac41440352f3d78b78340c
BugHunter-X <askzenai@gmail.com>
2024-12-20 17:14:06 +0100

    Add MyComponentSolution.js file

---
+++ MyComponentSolution.js
@@ -0,0 +1,12 @@
+```javascript
+// MyComponentSolution.js
+import React from 'react';
+
+export default function MyComponent() {
+  return (
+    <div className="bg-gradient-to-r from-blue-500 via-blue-600 to-purple-500 p-4">
+      {/* Some content here */}
+    </div>
+  );
+}
+```
