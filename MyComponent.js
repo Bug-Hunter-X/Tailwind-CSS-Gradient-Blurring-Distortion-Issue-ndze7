df51729d75426f8ffb5916007833457fd7817465
BugHunter-X <askzenai@gmail.com>
2024-12-20 17:14:05 +0100

    Add MyComponent.js file

---
+++ MyComponent.js
@@ -0,0 +1,12 @@
+```javascript
+// MyComponent.js
+import React from 'react';
+
+export default function MyComponent() {
+  return (
+    <div className="bg-gradient-to-r from-blue-500 to-purple-500 p-4">
+      {/* Some content here */}
+    </div>
+  );
+}
+```
